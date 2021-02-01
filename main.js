1fe4dd1078cf4b844d1f03a617145335dc0f2158
DaryaRockh <77782246+DaryaRockh@users.noreply.github.com>
2021-02-01 15:53:34 +0300

    Files downloaded
    Files downloaded from my PC

---
+++ main.js
@@ -0,0 +1,27 @@
+/*letting read more*/
+
+document.addEventListener('click', function(event) {
+    let id = event.target.dataset.toggleId;
+    if (!id) return;
+    let elem = document.getElementById(id);
+    elem.hidden = !elem.hidden;
+  });
+
+let watchAll = document.getElementById("watchAll")
+let newsBlockMore = document.getElementById("newsBlockMore")
+
+watchAll.addEventListener('click', function () {
+  /*newsBlockMore.hidden = !newsBlockMore.hidden;*/
+  newsBlockMore.style.display = "block";
+  watchAll.style.display = "none";
+},);
+
+
+
+
+
+
+
+
+
+
